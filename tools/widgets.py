971457a1a80d134e95930d83aafff9de46eeea1f
sergio-chumacero <sergio.chumacer.fi@gmail.com>
2019-03-10 01:38:14 -0400

    added report generator first version

--- tools/widgets.py
+++ tools/widgets.py
@@ -4,6 +4,124 @@ from os.path import dirname as up
 import json
 import requests
 
+import docx
+from datetime import datetime
+
+class GenerateReportWidget(widgets.VBox):
+    def __init__(self):
+        home_dir = os.path.join(os.environ['USERPROFILE'],'aapslab')
+        report_profile_path = os.path.join(home_dir,'.profile','report_profile.json')
+        model_path = os.path.join(home_dir,'tools','models','modelo_poa.docx')
+        out_path = os.path.join(home_dir,'datos','reportes')
+
+        if os.path.exists(report_profile_path):
+            with open(report_profile_path,'r') as f:
+                report_profile_json = json.load(f)
+        else:
+            report_profile_json = {}
+
+        report_number = widgets.BoundedIntText(
+            value= report_profile_json.get('last_report_num',0) + 1,
+            min=0, max=999, step=1,
+            description='Número de reporte:',
+            tooltip='entre 0 y 999',
+            layout=widgets.Layout(width='50%',),
+            style={'description_width': 'initial'},
+        )
+
+        name_text = widgets.Text(
+            value = report_profile_json.get('name'),
+            placeholder='Nombre del autor del documento',
+            description='Nombre:',
+            layout=widgets.Layout(width='50%',),
+        )
+
+        specialty_text = widgets.Text(
+            value = report_profile_json.get('specialty'),
+            placeholder='Especialidad del profesional',
+            description='Especialidad:',
+            layout=widgets.Layout(width='50%',),
+        )
+
+        qualification_type = widgets.ToggleButtons(
+            value=report_profile_json.get('prof'),
+            options=['Ingeniero', 'Económico',],
+            description='Profesión:',
+            button_style='info',
+            tooltips=['Profesional Ingeniero', 'Profesional Económico',],
+        )
+
+        generate_button = widgets.Button(
+            description='Generar Reporte',
+            button_style='success',
+            tooltip='Generar Reporte',
+            icon='file-text',
+        )
+
+        save_profile_button = widgets.Button(
+            description='Guardar Perfil',
+            button_style='info',
+            tooltip='Generar Reporte',
+            icon='save',
+        ) 
+
+        help_html = widgets.HTML()
+
+        prof_name_to_denom = {'Ingeniero':'Ing.', 'Económico':'Lic.',}
+
+        now = datetime.now()
+        month_names = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+        month_num_to_name = {k+1:v for k,v in zip(range(12), month_names)}
+
+        def on_generate_button_click(b):
+            
+            if os.path.exists(model_path):
+                test_doc = docx.Document(model_path)
+            else:
+                help_html.value = '<font color="red">No se encontró el modelo base de POA!</font>'
+                return
+            
+            test_doc.paragraphs[3].text = f'AAPS/DER/INF/{report_number.value:03d}/2019'
+
+            prof = qualification_type.value
+            denom = prof_name_to_denom[prof]
+
+            test_doc.tables[0].columns[2].cells[2].paragraphs[3].text = f'{denom} {name_text.value}'.title()
+            test_doc.tables[0].columns[2].cells[2].paragraphs[4].text = f'{prof} {specialty_text.value}'.upper()
+            test_doc.tables[0].columns[2].cells[5].paragraphs[0].text = f'La Paz, {now.day} de {month_num_to_name[now.month]} de {now.year}'
+
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+                
+            test_doc.save(os.path.join(out_path,f'reporte_poa_{month_num_to_name[now.month]}_{now.day}_{now.hour}_{now.minute}.docx'))
+
+            help_html.value = 'Informe generado y guardado en la carpeta <code>datos/reportes</code>!</br>Puedes descargar los reportes desde el navegador (<a href="http://localhost:8888/tree/datos/reportes"><font color="blue">LINK</font></a>) o acceder a ellos directamente a la carpeta en tu ordenador.'
+
+        def on_save_profile_button_click(b):
+            try:
+                if not os.path.exists(os.path.dirname(report_profile_path)):
+                    os.makedirs(os.path.dirnaem(report_profile_path))
+
+                report_profile_json = dict(
+                    name=name_text.value,
+                    prof=qualification_type.value,
+                    specialty=specialty_text.value,
+                    last_report_num=report_number.value,
+                )
+
+                with open(report_profile_path,'w') as f:
+                    json.dump(report_profile_json,f)
+
+                help_html.value = 'Perfil guardado!'
+            except Exception as e:
+                help_html.value = str(e)
+
+        generate_button.on_click(on_generate_button_click)
+        save_profile_button.on_click(on_save_profile_button_click)
+
+        super().__init__(children=[name_text, qualification_type, specialty_text, report_number, widgets.HBox([generate_button,save_profile_button]),help_html])
+        
+
 class LoginWidget(widgets.VBox):
     
     def __init__(self):
